3df378a4c2b3d395a663d68d7c39e4d1eef3e697
GoodbyeLullaby <smyhrps@163.com>
2020-02-23 09:42:49 +0800

    update day1

--- src/main/java/com/ustc/community/controller/HelloController.java
+++ src/main/java/com/ustc/community/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.ustc.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -12,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class HelloController {
-	@GetMapping("/hello")
-	public String hello(@RequestParam(name="name")String name, Model model){
-		model.addAttribute("name", name);
-		return "hello";
+	@GetMapping("/")
+	public String hello(){
+		return "index";
 	}
 }
